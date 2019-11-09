883deb09dc2465196bf7e48c3afe6c35eac8f9d3
JamiKr <jamikrueger@yahoo.com>
2019-11-09 11:28:08 -0800

    Added comments + template code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are functions which handle the inversion and caching of
+## inversion of matrices
 
-## Write a short comment describing this function
+## Set of functions to manage the matrix inversion cache.
+## Use this function to set up the matrix with the methods.  Make
+## sure you assign the result to a new R object, then pass that
+## resulting R object into cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## Matrix inversion function wrapper which first checks for
+## answer in cache before continuing
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
 }
